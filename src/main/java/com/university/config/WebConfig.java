51c99df33505bb9607004cdc5c6ff8d4394ac2b5
Alexander Semenets <satoshisadie@gmail.com>
2014-10-24 21:05:22 +0300

    Add freemarker configuration

--- src/main/java/com/university/config/WebConfig.java
+++ src/main/java/com/university/config/WebConfig.java
@@ -1,17 +1,49 @@
 package com.university.config;
 
+import freemarker.template.TemplateException;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.ui.freemarker.FreeMarkerConfigurationFactory;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
+
+import java.io.IOException;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = { "com.university.controllers" })
 public class WebConfig extends WebMvcConfigurerAdapter {
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        super.addResourceHandlers(registry);
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        super.addResourceHandlers(registry);
+//    }
+
+    @Bean FreeMarkerConfigurer freeMarkerConfigurer() {
+        final FreeMarkerConfigurationFactory factory = new FreeMarkerConfigurationFactory();
+        factory.setTemplateLoaderPath("/templates");
+
+        final FreeMarkerConfigurer freeMarkerConfigurer = new FreeMarkerConfigurer();
+        try {
+            freeMarkerConfigurer.setConfiguration(factory.createConfiguration());
+        } catch (IOException | TemplateException e) {
+            throw new RuntimeException(e);
+        }
+
+        return freeMarkerConfigurer;
+    }
+
+    @Bean FreeMarkerViewResolver freeMarkerViewResolver() {
+        final FreeMarkerViewResolver freeMarkerViewResolver = new FreeMarkerViewResolver();
+
+        // TODO Semenets Change this value in future
+        freeMarkerViewResolver.setCache(false);
+        freeMarkerViewResolver.setPrefix("");
+        freeMarkerViewResolver.setSuffix(".ftl");
+
+        return freeMarkerViewResolver;
     }
 }
--- src/main/java/com/university/controllers/admin/AdminController.java
+++ src/main/java/com/university/controllers/admin/AdminController.java
@@ -3,13 +3,13 @@ package com.university.controllers.admin;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AdminController {
 
     @RequestMapping("/admin/home")
-    @ResponseBody
-    public String home() {
-        return "This is admin home";
+    public ModelAndView home() {
+        return new ModelAndView("test");
     }
 }
